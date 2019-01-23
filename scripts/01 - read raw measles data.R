994f116dd4c4612e140b89d3f3ee0a2e84856fac
kassteele <jan.van.de.kassteele@rivm.nl>
2019-01-23 13:37:57 +0100

    Updated script header comments

--- scripts/01 - read raw measles data.R
+++ scripts/01 - read raw measles data.R
@@ -1,4 +1,11 @@
 #
+# Nowcasting the number of new symptomatic cases during infectious
+# disease outbreaks using constrained P-spline smoothing
+#
+# Jan van de Kassteele - RIVM
+#
+
+#
 # Init ----
 #
 
